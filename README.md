fadf65f8b3cd37ecc1b8b4b1aaaff303dd745918
Omkar2240 <omkarramgirwar123@gmail.com>
2026-03-15 12:51:56 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Water Intake For Summers
+
+This application will tell you about How much water you need to intake to stay hydrated in summers 2026.
+
+
+1) User will input -> age, sex, activity level
+2) feed this user inputs to model.
+3) update the prompt to take the input. (add summer 40°C temp)
+4) return the output, and display it to the frontend.
+5) deploy it.
+
+
+
+
+
+# What we are using?
+
+1) Hugging face interface
+2) model="mistralai/Mistral-7B-Instruct-v0.2:featherless-ai",
---
+++ src/agent.py
@@ -0,0 +1,79 @@
+# from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+
+
+
+# llm = HuggingFaceEndpoint(
+#     repo_id="mistralai/Mistral-7B-Instruct-v0.2",
+#     huggingfacehub_api_token="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",
+#     task="text-generation",
+#     max_new_tokens=100
+# )
+
+# chat_model = ChatHuggingFace(llm=llm)
+
+# response = chat_model.invoke("Explain recursion simply")
+
+# print(response.content)
+
+
+
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="https://router.huggingface.co/v1",
+    api_key=os.environ["HF_TOKEN"],
+)
+
+# completion = client.chat.completions.create(
+#     model="mistralai/Mistral-7B-Instruct-v0.2:featherless-ai",
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": "What is the capital of France?"
+#         }
+#     ],
+# )
+
+# print(completion.choices[0].message)
+
+# token usage
+# print("Prompt tokens:", completion.usage.prompt_tokens)
+# print("Completion tokens:", completion.usage.completion_tokens)
+# print("Total tokens:", completion.usage.total_tokens)
+
+
+class WaterIntakeAgent:
+    def __init__(self):
+        self.history = []
+        
+    def analyze_intake(self, intake_ml, age, gender, activity_level, curr_temp):
+        
+        prompt = f"""In one short paragraph, analyze if {intake_ml} ml daily water intake is sufficient for a healthy adult, 
+        with the following characteristics: age {age}, gender {gender}, physical activity level {activity_level}, 
+        and climate {curr_temp}. Avoid disclaimers and repetition. Adjust context in 100 tokens or less."""
+        
+        response = client.chat.completions.create(
+            model="mistralai/Mistral-7B-Instruct-v0.2:featherless-ai",
+            max_tokens=150,
+            temperature=0.5,
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ], 
+        )
+        return response.choices[0].message.content
+    
+    
+    
+if __name__ == "__main__":
+    agent = WaterIntakeAgent()
+    intake_ml = 1500
+    age = 30
+    gender = "male"
+    activity_level = "moderate"
+    curr_temp = "40°C"
+    insights = agent.analyze_intake(intake_ml, age, gender, activity_level, curr_temp)
+    print(insights)
